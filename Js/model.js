75276eabe88bd5bf768627ffe59c3f168adda006
Kolakharemonika <kolakharemonika@gmail.com>
2023-09-06 17:00:55 +0530

    some other changes

--- Js/model.js
+++ Js/model.js
@@ -1,11 +1,76 @@
-// export const music = {
-//     songImg: '',
-//     songTitle: '',
-//     songWriter: ''
-// }
-export const loadRecipe = async function (recipeId) {
 
-    console.log('jhwfgjwyhgfbmesevfejhsfbjs,nc ', recipeId);
+export const music = {
+    songImg: '',
+    songTitle: '',
+    songWriter: ''
+}
 
+export const musicList = [{
+    songImg: 'hukum-170px.jpg',
+    songTitle: 'hukum',
+    songWriter: 'Anirudh Ravichander'
+}, {
+    songImg: 'heeriye-170px.jpg',
+    songTitle: 'Heeriye(feet. Arijit singh)',
+    songWriter: 'Jasleen Royal, Arijit singh'
+}, {
+    songImg: 'uddjaa-170px.jpg',
+    songTitle: 'Udd jaa kaale kaava',
+    songWriter: 'Udit Narayan'
+},
+{
+    songImg: 'hukum-170px.jpg',
+    songTitle: 'hukum',
+    songWriter: 'Anirudh Ravichander'
+}, {
+    songImg: 'heeriye-170px.jpg',
+    songTitle: 'Heeriye(feet. Arijit singh)',
+    songWriter: 'Jasleen Royal, Arijit singh'
+}, {
+    songImg: 'uddjaa-170px.jpg',
+    songTitle: 'Udd jaa kaale kaava',
+    songWriter: 'Udit Narayan'
+}]
+
+
+export const getMusicList = async function () {
+    return musicList.slice();
+}
+
+export const loadGallary = async function () {
+
+    console.log('jhwfgjwyhgfbmesevfejhsfbjs,nc ');
 
 }
+
+export const trendingSongMarkup = async function () {
+    const list = musicList.slice();
+    let html = `${musicList.map(music => {
+
+        return `<div class="song_card">
+            <img class="song-img" src="./assests/images/${music.songImg}" alt="${music.songTitle}">
+                <h5 class="song-title">${music.songTitle.padEnd(15, '.')}</h5>
+                <span class="song-writer">${music.songWriter}</span>
+        </div>`
+    }).join('')} `;
+
+    return html;
+}
+
+// export const loadGallary = async function () {
+//     console.log(music);
+//     let html = `${music.map(recipe => {
+//         return ` <button class="btn--recipes " id="${recipe.id}"><div class="recipee flex" >
+//                     <div class="recipe-img flex">
+
+//                         <img src="${recipe.imageUrl}" alt="${recipe.title}">
+//                     </div>
+//                     <span class="flex-row pt-10 recipe-dec">
+//                         <span class="heading--3">${recipe.title?.length >= 30 ? (recipe.title.slice(0, 30)).toUpperCase() + '...' : (recipe.title).toUpperCase()}</span>
+//                         <span>${recipe.publisher}</span>
+//                     </span>
+//                 </div>  </button>`
+//     }).join('')} `;
+
+//     return html;
+// }
--- script.js
+++ script.js
@@ -2,11 +2,23 @@ import * as model from './Js/model.js';
 const cardEl = document.querySelectorAll('.card');
 const showLyricsBtn = document.querySelector('.show_Lyrics_btn');
 const navBtns = document.querySelectorAll('.nav_item_click');
+
+const trendingSongsEl = document.querySelector('.trending_songs_cards');
+
+const musicPlayBtn = document.querySelector('.music_play_btn');
 const navList = document.querySelector('.nav_list')
 const trendingSongCard = document.querySelector('.trending_song_cards')
 
-// console.log(model.music);
-console.log(model.loadRecipe(2));
+
+console.log(await model.getMusicList());
+
+const trendingSongsList = await model.getMusicList();
+
+async function getTrendingSongsList() {
+    trendingSongsEl.innerHTML = await model.trendingSongMarkup();
+}
+// getTrendingSongsList();
+
 // function changeImage(move) { //working code slides 3
 //     const imgCards = document.querySelectorAll('.img_card');
 //     console.log(imgCards);
@@ -27,6 +39,7 @@ function trending() {
 }
 trending();
 const imgCards = document.querySelectorAll('.img_card');
+
 function changeImage(move) {
     var moveTo = 50;
 
@@ -47,9 +60,10 @@ function showHidwLyrics() {
     console.log('showLyricsBtn');
     showLyricsBtn.textContent = showLyricsBtn.textContent == 'Hide Lyrics' ? 'Show Lyrics' : 'Hide Lyrics';
     document.querySelector('.lyrics_sec').classList.toggle('hidden');
+    document.querySelector('.show_Lyrics_btn').classList.toggle('hide_Lyrics_btn');
 }
 
-showLyricsBtn.addEventListener('click', showHidwLyrics)
+showLyricsBtn.addEventListener('click', showHidwLyrics);
 document.querySelector('.expand_btn').addEventListener('click', showHidwLyrics);
 
 
@@ -60,8 +74,24 @@ navList.addEventListener('click', (e) => {
         navbtn.classList.remove('active');
     })
     e.target.closest('.nav_item_click').classList.add('active');
-})
+});
 
 document.querySelector(".dropbtn").addEventListener('click', () => {
     document.querySelector("#profileDropdown").classList.toggle("show");
-})
+});
+
+document.getElementById('musicPlay').addEventListener('click', () => {
+    document.querySelector(".song_play_container").classList.toggle("hidden");
+});
+
+musicPlayBtn.addEventListener('click', (e) => {
+    // console.log(e.target.closest('.music_play_btn_svg'));
+
+    console.log(e.target);
+    if (e.target.closest('.music_play_btn_svg')) {
+        console.log(e.target.closest('.music_play_btn_svg').getAttribute('href'));
+
+        musicPlayBtn.classList.toggle("pause");
+    }
+
+});
